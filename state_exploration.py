eb91e71604507b4a6701c7d1a2125e1cb4ca740f
Sadie Allen <sadiela@bu.edu>
2023-04-28 09:19:15 -0400

    fixed error in chord definition

--- state_exploration.py
+++ state_exploration.py
@@ -2,6 +2,10 @@
 import random
 import itertools
 import yaml
+import pretty_midi
+
+# pretty_midi.note_number_to_name(note_number) CHECK DICTS!
+
 
 bass_range = list(range(40,61))
 tenor_range = list(range(48,68)) 
@@ -20,7 +24,7 @@ notes_in_chords = {
     1: [Cs, Es, Gs],
     2: [Ds, Fs, As],
     3: [Es, Gs, Bs],
-    4: [Es, As, Cs],
+    4: [Fs, As, Cs],
     5: [Gs, Bs, Ds],
     6: [As, Cs, Es],
     7: [Bs, Ds, Fs]
@@ -73,54 +77,82 @@ def gen_triad_options(chord_num, double_note=1):
     # DIMINISHED HARMONIES NEED DOUBLED 3RDS
     return chord_options
 
+if __name__ == "__main__":
+    print("As:")
+    for a in As: 
+        print(pretty_midi.note_number_to_name(a))
 
-chord_1_options = gen_triad_options(1) + gen_triad_options(1, double_note=5)
-chord_1_options.sort()
-chord_1_options = list(chord_1_options for chord_1_options,_ in itertools.groupby(chord_1_options))
+    print("Bs:")
+    for b in Bs: 
+        print(pretty_midi.note_number_to_name(b))
 
-chord_2_options = gen_triad_options(2) + gen_triad_options(2, double_note=5)
-chord_2_options.sort()
-chord_2_options = list(chord_2_options for chord_2_options,_ in itertools.groupby(chord_2_options))
+    print("Cs:")
+    for c in Cs: 
+        print(pretty_midi.note_number_to_name(c))
 
-chord_3_options = gen_triad_options(3) + gen_triad_options(3, double_note=5)
-chord_3_options.sort()
-chord_3_options = list(chord_3_options for chord_3_options,_ in itertools.groupby(chord_3_options))
+    print("Ds:")
+    for d in Ds: 
+        print(pretty_midi.note_number_to_name(d))
 
-chord_4_options = gen_triad_options(4) + gen_triad_options(4, double_note=5)
-chord_4_options.sort()
-chord_4_options = list(chord_4_options for chord_4_options,_ in itertools.groupby(chord_4_options))
+    print("Es:")
+    for e in Es: 
+        print(pretty_midi.note_number_to_name(e))
 
-chord_5_options = gen_triad_options(5) + gen_triad_options(5, double_note=5)
-chord_5_options.sort()
-chord_5_options = list(chord_5_options for chord_5_options,_ in itertools.groupby(chord_5_options))
+    print("Fs:")
+    for f in Fs: 
+        print(pretty_midi.note_number_to_name(f))
 
-chord_6_options = gen_triad_options(6) + gen_triad_options(6, double_note=5)
-chord_6_options.sort()
-chord_6_options = list(chord_6_options for chord_6_options,_ in itertools.groupby(chord_6_options))
+    print("Gs:")
+    for g in Gs: 
+        print(pretty_midi.note_number_to_name(g))
 
-chord_7_options = gen_triad_options(7, double_note=3) # DIMINISHED! Double the third
+    chord_1_options = gen_triad_options(1) + gen_triad_options(1, double_note=5)
+    chord_1_options.sort()
+    chord_1_options = list(chord_1_options for chord_1_options,_ in itertools.groupby(chord_1_options))
 
-all_chord_options = [chord_1_options, chord_2_options, chord_3_options, chord_4_options, chord_5_options, chord_6_options, chord_7_options]
+    chord_2_options = gen_triad_options(2) + gen_triad_options(2, double_note=5)
+    chord_2_options.sort()
+    chord_2_options = list(chord_2_options for chord_2_options,_ in itertools.groupby(chord_2_options))
 
-state_dict = {}
-state_indices = {}
+    chord_3_options = gen_triad_options(3) + gen_triad_options(3, double_note=5)
+    chord_3_options.sort()
+    chord_3_options = list(chord_3_options for chord_3_options,_ in itertools.groupby(chord_3_options))
 
-index = 0
-for chord_options in all_chord_options:
-    for c_opt in chord_options:
-        state_indices[index] = c_opt
-        index+=1
+    chord_4_options = gen_triad_options(4) + gen_triad_options(4, double_note=5)
+    chord_4_options.sort()
+    chord_4_options = list(chord_4_options for chord_4_options,_ in itertools.groupby(chord_4_options))
 
-for i, chord_options in enumerate(all_chord_options):
-    state_dict[i+1] = []
-    for j, c_opt in enumerate(chord_options):
-        key = next(key for key, value in state_indices.items() if value == c_opt)
-        #print(i, j, c_opt)
-        name = "chord_" + str(i+1) + '_' + str(j)
-        state_dict[i+1].append(key)
+    chord_5_options = gen_triad_options(5) + gen_triad_options(5, double_note=5)
+    chord_5_options.sort()
+    chord_5_options = list(chord_5_options for chord_5_options,_ in itertools.groupby(chord_5_options))
 
-with open("voicing_state_dict.yaml", 'w') as outfile:
-    yaml.dump(state_dict, outfile, default_flow_style=False)
+    chord_6_options = gen_triad_options(6) + gen_triad_options(6, double_note=5)
+    chord_6_options.sort()
+    chord_6_options = list(chord_6_options for chord_6_options,_ in itertools.groupby(chord_6_options))
 
-with open("voicing_state_indices.yaml", 'w') as outfile:
-    yaml.dump(state_indices, outfile, default_flow_style=False)
+    chord_7_options = gen_triad_options(7, double_note=3) # DIMINISHED! Double the third
+
+    all_chord_options = [chord_1_options, chord_2_options, chord_3_options, chord_4_options, chord_5_options, chord_6_options, chord_7_options]
+
+    state_dict = {}
+    state_indices = {}
+
+    index = 0
+    for chord_options in all_chord_options:
+        for c_opt in chord_options:
+            state_indices[index] = c_opt
+            index+=1
+
+    for i, chord_options in enumerate(all_chord_options):
+        state_dict[i+1] = []
+        for j, c_opt in enumerate(chord_options):
+            key = next(key for key, value in state_indices.items() if value == c_opt)
+            #print(i, j, c_opt)
+            name = "chord_" + str(i+1) + '_' + str(j)
+            state_dict[i+1].append(key)
+
+    with open("voicing_state_dict.yaml", 'w') as outfile:
+        yaml.dump(state_dict, outfile, default_flow_style=False)
+
+    with open("voicing_state_indices.yaml", 'w') as outfile:
+        yaml.dump(state_indices, outfile, default_flow_style=False)
